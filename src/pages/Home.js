0036077286bb3dbb6db1a583a5ab5f1c6f990e3a
atulkp018 <atulkp018@gmail.com>
2024-08-22 12:09:02 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>
+      Please Log in or Sign up to continue......
+    </div>
+  )
+}
+
+export default Home
